38616c2b744d1ef3d20d23c6b1455f467d8aafa2
Jeavin Shoker <jshokerwow@gmail.com>
2020-02-26 20:32:55 -0800

    Finished lab5

---
+++ app.js
@@ -0,0 +1,76 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const hostname = '127.0.0.1';
+const port = 3000;
+jsonPath = __dirname + '/profiles.json';
+
+app.use(express.static(__dirname));
+
+//Starting the server
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/lab02.html'));
+});
+app.listen(8888, () => console.log('server started at port 8888'));
+
+//load artists
+app.get('/loadProfiles', function(req, res) {
+  var artist = null;
+  fs.readFile(jsonPath, (err, data) => {
+    if (err) {throw err};
+    artist = JSON.parse(data);
+    console.log(artist);
+    res.json(artist);
+  });
+});
+
+//Reading from the file
+'use strict';
+fs.readFile(jsonPath, (err, data) => {
+    let artistArray = JSON.parse(data);
+    console.log(artistArray);
+});
+console.log('This is after the read call');
+
+//Reading and writing to the file
+app.post('/profiles', function(req, res) {
+  var name = req.body;
+  // console.log(name);
+  var element = JSON.parse(JSON.stringify(name));
+  console.log(element);
+  fs.readFile(jsonPath, (err, data) => {
+      let artistArray = JSON.parse(data);
+      element["id"] = artistArray.length;
+      artistArray.push(element);
+      console.log(artistArray);
+      fs.writeFile(jsonPath, JSON.stringify(artistArray), function(err){
+        if (err) {throw err}
+      });
+  });
+  res.end("yes");
+});
+console.log('This is after the read/write call');
+
+//Deleting
+app.post('/delete', function(req, res) {
+    var name = req.body;
+    var element = JSON.parse(JSON.stringify(name));
+    fs.readFile(jsonPath, (err, data) => {
+        if (err) throw err;
+        let artistArray = JSON.parse(data);
+        artistArray.splice(parseInt(element.ind), 1);
+        for(let i = 0; i < artistArray.length; i++){
+            artistArray[i].id = i;
+        }
+        console.log(artistArray);
+        fs.writeFile(jsonPath, JSON.stringify(artistArray), function(err) {
+            if (err) { throw err }
+        });
+    });
+});
--- js/lab02.js
+++ js/lab02.js
@@ -2,6 +2,7 @@ var displayArtistForm = document.getElementById("artistFormDiv");
 var artistArray = [];
 var size = 0;
 
+
 loadArtistList();
 
 function toggleArtistForm() {
@@ -13,131 +14,153 @@ function toggleArtistForm() {
 }
 
 function loadArtistList() {
-  let arr = localStorage.getItem('artistArray');
-  console.log(arr);
-  let profiles = JSON.parse(arr);
-  size = profiles.length;
-  console.log(profiles);
-  console.log(size);
-
-  for(let i = 0; i < size; i++){
-    createProfile(i);
-  }
+  fetch('/loadProfiles', {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+    'Content-Type': 'application/json'
+    }
+   })
+   .then((res) => {
+        return res.json();
+   })
+   .then((data) => {
+        console.log(data)
+        clearArtistList();
+        createProfile(data);
+   })
 }
 
-function insertArray(name, desc, img){
-
-  console.log(profileItem);
-
-  if(JSON.parse(localStorage.getItem('artistArray'))){
-    temp = JSON.parse(localStorage.getItem('artistArray'));
-    var profileItem = {name: name, desc: desc, img: img};
-    temp.push(profileItem);
-    localStorage.setItem('artistArray', JSON.stringify(temp));
-  } else {
-    var profileItem = [{name: name, desc: desc, img: img}];
-    localStorage.setItem('artistArray', JSON.stringify(profileItem));
-  }
-  loadArtistList();
+function clearArtistList(){
+    var x = document.getElementById('peopleList');
+    var child = x.lastElementChild;
+    while (child) {
+        x.removeChild(child);
+        child = x.lastElementChild;
+    }
 }
+
+// function insertArray(name, desc, img){
+//
+//   // if(JSON.parse(localStorage.getItem('artistArray'))){
+//   //   temp = JSON.parse(localStorage.getItem('artistArray'));
+//   //   var profileItem = {name: name, desc: desc, img: img};
+//   //   temp.push(profileItem);
+//   //   localStorage.setItem('artistArray', JSON.stringify(temp));
+//   // } else {
+//   //   var profileItem = [{name: name, desc: desc, img: img}];
+//   //   localStorage.setItem('artistArray', JSON.stringify(profileItem));
+//   // }
+//   // loadArtistList();
+// }
 
 function addArtist() {
-
     var profile = document.getElementById("addForm").elements;
     let profileName = profile[0].value;
     let profileDesc = profile[1].value;
     let profileImg = profile[2].value;
 
-    insertArray(profileName, profileDesc, profileImg);
+    var profileJSON = {"name":profileName, "desc":profileDesc, "img":profileImg};
+    console.log(profileJSON);
+
+    fetch('/profiles', {
+      method: 'POST',
+      body: JSON.stringify(profileJSON),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json)
+    .then(json => console.log(json))
+    .then((data) => {
+      loadArtistList();
+    });
 
     displayArtistForm.style.display = "none";
-    // var ul = document.getElementById("peopleList");
-    // var li = document.createElement("li");
-    // var person = document.createElement("div");
-    // person.setAttribute("id", "person");
-    // ul.insertBefore(li, ul.firstChild);
-    // li.appendChild(person);
-    // var img = document.createElement("img");
-    // person.appendChild(img);
-    // img.setAttribute("id", "img");
-    // img.setAttribute("alt", profileName);
-    // img.setAttribute("src", profileImg);
-    // var info = document.createElement("div");
-    // info.setAttribute("class", "info");
-    // person.appendChild(info);
-    // var name = document.createElement("p");
-    // name.setAttribute("class", "name");
-    // var about = document.createElement("p");
-    // info.appendChild(name);
-    // info.appendChild(about);
-    // name.appendChild(document.createTextNode(profileName));
-    // about.appendChild(document.createTextNode(profileDesc));
-    // var del = document.createElement("button");
-    // del.setAttribute("class", "del");
-    // del.setAttribute("onClick", "deleteArtist(this)");
-    // person.appendChild(del);
-    // del.appendChild(document.createTextNode("Delete"));
 }
 
-function createProfile(i) {
+function createProfile(jsonArray) {
 
-  // let profileName = profile[0].value;
-  // let profileDesc = profile[1].value;
-  // let profileImg = profile[2].value;
-  // temp1 = localStorage.getItem('artistArray');
-  // console.log(temp1);
+  id = 0;
+  for(let i = 0; i < jsonArray.length; i++){
+    var name = "";
+    var desc = "";
+    var url = "";
+    var id = 0;
+    name = jsonArray[i].name;
+    desc = jsonArray[i].desc;
+    url = jsonArray[i].img;
+    id = jsonArray[i].id;
 
-  let artist = JSON.parse(localStorage.getItem('artistArray'));
-  console.log(artist);
+    console.log(name);
+    console.log(desc);
+    console.log(url);
+    console.log(id);
 
-  var ul = document.getElementById("peopleList");
-  var li = document.createElement("li");
-  var person = document.createElement("div");
-  person.setAttribute("id", "person");
-  ul.insertBefore(li, ul.firstChild);
-  li.appendChild(person);
-  var img = document.createElement("img");
-  person.appendChild(img);
-  img.setAttribute("id", "img");
-  img.setAttribute("alt", artist[i].name);
-  img.setAttribute("src", artist[i].img);
-  var info = document.createElement("div");
-  info.setAttribute("class", "info");
-  person.appendChild(info);
-  var name = document.createElement("p");
-  name.setAttribute("class", "name");
-  var about = document.createElement("p");
-  info.appendChild(name);
-  info.appendChild(about);
-  name.appendChild(document.createTextNode(artist[i].name));
-  about.appendChild(document.createTextNode(artist[i].desc));
-  var del = document.createElement("button");
-  del.setAttribute("class", "del");
-  del.setAttribute("onClick", "deleteArtist(this)");
-  person.appendChild(del);
-  del.appendChild(document.createTextNode("Delete"));
+    var ul = document.getElementById("peopleList");
+    var li = document.createElement("li");
+    var person = document.createElement("div");
+    person.setAttribute("id", "person");
+    ul.insertBefore(li, ul.firstChild);
+    li.appendChild(person);
+    var img = document.createElement("img");
+    person.appendChild(img);
+    img.setAttribute("id", "img");
+    img.setAttribute("alt", name);
+    img.setAttribute("src", url);
+    var info = document.createElement("div");
+    info.setAttribute("class", "info");
+    person.appendChild(info);
+    var name2 = document.createElement("p");
+    name2.setAttribute("class", "name");
+    var about = document.createElement("p");
+    info.appendChild(name2);
+    info.appendChild(about);
+    name2.appendChild(document.createTextNode(name));
+    about.appendChild(document.createTextNode(desc));
+    var del = document.createElement("button");
+    del.setAttribute("class", "del");
+    del.setAttribute("onClick", "deleteArtist(this)");
+    del.setAttribute("value", id);
+    person.appendChild(del);
+    del.appendChild(document.createTextNode("Delete"));
+  }
 }
 
 function deleteArtist(btn){
-    (btn.parentNode.parentNode.parentNode).removeChild(btn.parentNode.parentNode);
+    var id = btn.value;
+    console.log(id);
+    let index = {
+        ind: id
+        };
+    console.log(index);
+    fetch('/delete', {
+        method: 'POST',
+        body: JSON.stringify(index),
+        headers: { 'Content-Type': 'application/json;' },
+    })
+    .then((data) => {
+        console.log(data);
+        loadArtistList();
+    })
+    .then(res => res.json);
+    loadArtistList();
 }
 
-function searchArray(){
-  let searchBar = document.getElementById("searchValue").value;
-  console.log(searchBar);
-  let artist = JSON.parse(localStorage.getItem('artistArray'));
-  let arr = new Array();
-  for(let i = 0; i < artist.length; i++){
-    if(artist[i].name.search(searchBar) >= 0){
-        arr.push(i);
-        console.log(artist[i].name);
-    }
-  }
-  let list = document.getElementById('peopleList');
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-  for(let profile of arr){
-    createProfile(profile);
-  }
-}
+// function searchArray(){
+//   let searchBar = document.getElementById("searchValue").value;
+//   console.log(searchBar);
+//   let artist = JSON.parse(localStorage.getItem('artistArray'));
+//   let arr = new Array();
+//   for(let i = 0; i < artist.length; i++){
+//     if(artist[i].name.search(searchBar) >= 0){
+//         arr.push(i);
+//         console.log(artist[i].name);
+//     }
+//   }
+//   let list = document.getElementById('peopleList');
+//   while (list.firstChild) {
+//     list.removeChild(list.firstChild);
+//   }
+//   for(let profile of arr){
+//     createProfile(profile);
+//   }
+// }
